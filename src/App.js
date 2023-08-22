b8e866950bb4bb25d9be619362f182e29ae2d6de
bhaveshbl9697 <bhaveshbl9697@gmail.com>
2023-08-22 14:10:20 +0530

    use state example commit

--- src/App.js
+++ src/App.js
@@ -2,35 +2,27 @@ import React, {  useState } from 'react';
 import './App.css';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  // let count = 0;
-  function handleAdd() {
-    // count += 1;
-    // console.log(count);
-    setCount(count+1);
+ const [tasks, setTasks] = useState([
+   { id: 5271, name: "record lecture", completed: false },
+   { id: 5272, name: "record react lecture", completed: true },
+   { id: 5273, name: "record javascript lecture", completed: false },
+ ]);
+  console.log(setTasks)
+  function handleDelete(id) {
+    setTasks(tasks.filter(task => id !== task.id));
   }
-  function handleSub() {
-      setCount(count - 1);
-  }
-  function handleReset() {
-    setCount(0);
-  }
-  
   return (
     <>
       <div className="App">
-        <div className="box">
-          <p>{count}</p>
-          <button className="add" onClick={handleAdd}>
-            ADD
-          </button>
-          <button className="sub" onClick={handleSub}>
-            SUB
-          </button>
-          <button className="reset" onClick={handleReset}>
-            RESET
-          </button>
-        </div>
+        <h1> TASK LIST</h1>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              <span>{task.id} - {task.name}</span>
+              <button onClick={()=>handleDelete(task.id)} className='delete'>delete</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
